a86e7c2364e6886983c9d6db15215e95ed364b72
Anifyuli <anifyuliansyah@gmail.com>
2021-06-23 20:38:50 +0700

    Make a raw of Login Pages

--- costumer/login.php
+++ costumer/login.php
@@ -5,21 +5,68 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Masuk</title>
-    <link rel="shortcut icon" href="./assets/img/login.svg" type="image/x-icon">
-    <link rel="stylesheet" href="./assets/ext/fontawesome5/css/all.css">
-    <link rel="stylesheet" href="./assets/ext/bootstrap4/css/bootstrap.css">
-    <link rel="stylesheet" href="./assets/ext/bootstrap4/css/bootstrap.css.map">
+    <link rel="shortcut icon" href="../costumer/assets/img/favicon.png" type="image/x-icon">
+    <link rel="stylesheet" href="../assets/ext/fontawesome5/css/all.css">
+    <link rel="stylesheet" href="../assets/ext/bootstrap4/css/bootstrap.css">
+    <link rel="stylesheet" href="../assets/ext/bootstrap4/css/bootstrap.css.map">
+    <link rel="stylesheet" href="../costumer/assets/mystyle/css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=PT+Sans&display=swap" rel="stylesheet"> 
 </head>
 <body>
-    <div class="d-flex p-2 bd-highlight">
-        <img src="../assets/img/logo.jpg" alt="">
-    </div>
-    <div id="photo">
-    
+    <!-- Image and text -->
+    <nav class="navbar navbar-light bg-light">
+    <a class="navbar-brand" href="#">
+        <img src="../costumer/assets/img/logo0.png" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy">
+        PT JAYA RAYA
+    </a>
+    </nav>
+    <div class="container-fluid">
+        <div id="photo">
+            <div class="img-1st">
+                <img src="../costumer/assets/img/img0.png" alt="">
+            <div>
+            <div class="img-2nd">
+                <img src="../costumer/assets/img/img1.jpeg" alt="">
+            </div>
+            <div class="img-3rd">
+                <img src="../costumer/assets/img/img0.png" alt="">
+            </div>
+        <div id="form">
+        <h3>LOGIN</h3>
+            <form>
+                <div class="form-group">
+                    <label for="exampleInputEmail1">
+                        EMAIL
+                    </label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputPassword1">
+                        PASSWORD
+                    </label>
+                    <input type="password" class="form-control" id="exampleInputPassword1">
+                </div>
+                <div class="form-group form-check">
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                    <label class="form-check-label" for="exampleCheck1">REMEMBER ME</label>
+                </div>
+                <div class="mb-3">
+                    <button type="submit" class="btn btn-success btn-lg btn-block">MASUK</button>
+                </div>
+                <div>
+                    <a href="#"><u>LUPA PASSWORD?</u></a>
+                </div>
+                <div class="mt-3">
+                    <button type="submit" class="btn btn-secondary btn-lg btn-block">DAFTAR</button>
+                </div>
+            </form>
+        </div>
     </div>
 
 
-    <script src="./assets/ext/fontawesome5/js/all.js"></script>
+    <script src="../assets/ext/fontawesome5/js/all.js"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
