bd88c83abc7c44cee3cbaa06555c6932db3fbb6c
Anifyuli <anifyuliansyah@gmail.com>
2021-07-24 23:02:01 +0700

    Adding component

--- index.php
+++ index.php
@@ -5,9 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Beranda</title>
+    <link rel="stylesheet" href="./costumer/assets/mystyle/css/index-style.css">
     <link rel="stylesheet" href="./costumer/assets/ext/bootstrap4/css/bootstrap.min.css">
     <link rel="stylesheet" href="./costumer/assets/ext/bootstrap4/css/bootstrap.min.css.map">
-    <link rel="stylesheet" href="../assets/ext/fontawesome5/css/all.css">
+    <link rel="stylesheet" href="./costumer/assets/ext/fontawesome5/css/all.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=PT+Sans&display=swap" rel="stylesheet">
@@ -15,10 +16,54 @@
 </head>
 <body>
     
+    <nav class="navbar navbar-expand-lg navbar-light sticky-top" style="background-color : rgba(168, 168, 168, 0.3);">
+    <a class="navbar-brand" href="#">SD MERDEKA</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
 
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="mx-auto navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link text-primary" href="#"><u>Beranda</u> <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-primary" href="#"><u>Profil</u></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-primary" href="#"><u>Galeri</u></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-primary" href="#"><u>Artikel</u></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-primary" href="#"><u>Kontak</u></a>
+            </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+        <a class="rounded-0 btn btn-outline-primary my-2 my-sm-0 px-5" href="">Masuk</a>
+        </form>
+    </div>
+    </nav>
 
+    <div id="jumbotron" class="jumbotron jumbotron-fluid" style="height : 350px;">
+        <div class="container">
+            <h1 style="font-size : 32px; margin-top : 55px;"><b>SEKOLAH DIGITAL <br> TARAF NASIONAL</b></h1>
+            <a class="btn btn-lg" href="#" style="color : rgb(83, 83, 83); background-color: rgba(168, 168, 168, 0.4); font-size : 20px;">Selengkapnya</a>
+        </div>
+    </div>
+
+    <div class="container-md" style="border : 2px solid rgb(83, 83, 83); ">
+        <div class="text-primary" style="list-style: none; display : flex; margin : 0 auto;">
+            <h5>570 SISWA</h5>
+            <h5>4 EKSKUL</h5>
+            <h5>33 GURU</h5>
+            <h5>3 GEDUNG</h5>
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="./costumer/assets/ext/bootstrap4/js/bootstrap.min.js"></script>
-    <script src="./costumer/assets/ext/bootstrap4/js/bootstrap.min.js.map"></script>
-
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 </body>
 </html>
